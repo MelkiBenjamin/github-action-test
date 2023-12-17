05363162ddaabf56d3acac0a2c9bb309cfa0edaa
benjamin melki <mbenjaminmelki@gmail.com>
2023-12-17 22:34:33 +0100

    test

--- script.py
+++ script.py
@@ -5,16 +5,16 @@ print("hello world")
 
 memrestant = psutil.virtual_memory().percent #memoire restante disponible en %
 print(memrestant)
-print("Bonjour vous avez ",memrestant," % de memoire restante disponible.")
+print("Bonjour vous avez ",memrestant,"% de memoire restante disponible.")
 
 memutilisé = 100-psutil.virtual_memory().percent  #memoire utliser en %
 print(memutilisé)
-print("Bonjour vous utiliser ",memutilisé," % de memoire.")
+print("Bonjour vous utiliser ",memutilisé,"% de memoire.")
 
 memoirerest = 16*memutilisé/100 #memoire utiliser
 print(math.ceil(memoirerest))
 memar = math.ceil(memoirerest) #memoire utlisé arrondit
 print(memar)
-print("Bonjour vous utiliser ",memar," de memoire.")
+print("Bonjour vous utiliser ",memar,"GO de memoire.")
 
 if memutilisé > 65 : print ("Attention, vous utliser trop de memoire.")
