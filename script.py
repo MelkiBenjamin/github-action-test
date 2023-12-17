d7b2408443fe795a92410791bcc6873799d3908d
benjamin melki <mbenjaminmelki@gmail.com>
2023-12-17 22:20:33 +0100

    test

--- script.py
+++ script.py
@@ -3,15 +3,15 @@ import psutil
 print("hello world")
 
 memrestant = psutil.virtual_memory().percent #memoire restante disponible en %
-print("memrestant%")
+print(memrestant)
+print("Bonjour vous avez ",memrestant," % de memoire restante disponible.")
 
 memutilisé = 100-psutil.virtual_memory().percent  #memoire utliser en %
 print(memutilisé)
+print("Bonjour vous utiliser ",memutilisé," % de memoire.")
 
 memoirerest = 16*memutilisé/100 #memoire utiliser
-
 print(memoirerest)
+print("Bonjour vous utiliser ",memoirerest," de memoire.")
 
-print("bonjour vous utiliser",memoirerest,"% de memoire")
-
-if memutilisé > 65 : print ("vous utliser trop de memoire")
+if memutilisé > 65 : print ("vous utliser trop de memoire.")
